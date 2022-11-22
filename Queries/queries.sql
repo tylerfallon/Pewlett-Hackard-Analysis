02c0cd9e763a403a2af04dd0da29823d600fdade
tylerfallon <tylermfallon@gmail.com>
2022-11-22 04:50:11 -0500

    added another table

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -65,3 +65,7 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * from retirement_info;
+
+SELECT first_name, last_name, title
+FROM employees as e
+LEFT JOIN titles as t ON e.emp_no = t.emp_no
