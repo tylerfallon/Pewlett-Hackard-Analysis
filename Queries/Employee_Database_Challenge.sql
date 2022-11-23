d4bb68c26e3f207dc5c1795c8c091ac8763d83e6
tylerfallon <tylermfallon@gmail.com>
2022-11-23 01:04:34 -0500

    deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable 1
+
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date 
 INTO retirement_titles 
 FROM employees AS e   
@@ -14,9 +16,18 @@ ORDER BY emp_no, to_date DESC;
 
 SELECT title, COUNT(title) AS "title_count"
 INTO retirement_titles_count
-FROM retirement_titles
+FROM unique_titles
 GROUP BY title
 ORDER BY "title_count" DESC
 ;
 
-SELECT * FROM retirement_titles_count;
+-- Deliverable 2
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, de.from_date, de.to_date, ti.title
+INTO mentorship_eligibility 
+FROM employees AS e
+INNER JOIN dept_emp as de ON (de.emp_no = e.emp_no)
+INNER JOIN titles as ti ON (ti.emp_no = e.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+--Deliverable 3
